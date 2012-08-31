350339d803a0c2b17169a43a5d60f341a8f112eb
yuri@panchul.com <yuri@panchul.com@c79ba80f-ef26-09ec-9069-3de584f4e60f>
2012-08-31 02:19:37 +0000

    Added examples how to use PORTxSET, PORTxCLR, PORTxINV

--- trunk/tedious/001_blinking_led.X/main.c
+++ trunk/tedious/001_blinking_led.X/main.c
@@ -1,11 +1,19 @@
 /* 
  * File:   main.c
- * Author: panchul
+ * Author: Yuri Panchul
  *
  * Created on August 30, 2012, 3:42 PM
  */
 
 #include <p32xxxx.h>
+
+void delay (int n)
+{
+    n *= 10000;
+
+    while (n --)
+        asm ("nop");
+}
 
 void main (void)
 {
@@ -15,10 +23,60 @@ void main (void)
 
     for (;;)
     {
-        for (i = 0; i < 10000; i++)
-            asm ("nop");
-        
-        LATE = LATE + 1;
+        /* Output uses LAT register */
+
+        LATE = 0;
+
+        for (i = 0; i < 16; i++)
+        {
+            delay (1);
+            LATE ++;
+        }
+
+        LATE = 0;
+
+        for (i = 0; i < 256; i += 16)
+        {
+            delay (1);
+            LATE = i;
+        }
+
+        LATE = 0;
+
+        /* Output uses PORT register */
+
+        PORTE = 1;
+
+        for (i = 0; i < 8; i++)
+        {
+            delay (1);
+            PORTE <<= 1;
+        }
+
+        PORTE = 0;
+
+        /* Output uses PORTxSET, PORTxCLR and PORTxINV registers */
+
+        for (i = 0; i < 4; i++)
+        {
+            delay (1);
+            PORTESET = (1 << i) | (0x80 >> i);
+        }
+
+        for (i = 0; i < 4; i++)
+        {
+            delay (1);
+            PORTECLR = (1 << i) | (0x80 >> i);
+        }
+
+        delay (1);
+        PORTESET = 0xAA;
+
+        for (i = 0; i < 8; i++)
+        {
+            delay (1);
+            PORTEINV = 1 << i;
+        }
     }
 }
 
