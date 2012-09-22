bb679c676a9bfe62e46a136181bde7bd9eadbb21
yuri <yuri@panchul.com>
2012-09-22 02:04:01 +0000

    Added keypad example

---
+++ tedious/library/config.h
@@ -0,0 +1,9 @@
+//  File:   config.h
+//  Author: Yuri Panchul
+
+#ifndef CONFIG_H
+#define  CONFIG_H
+
+#define PBCLK_FREQUENCY (10 * 1000 * 1000)
+
+#endif
--- tedious/library/types.h
+++ tedious/library/types.h
@@ -4,6 +4,9 @@
 #ifndef TYPES_H
 #define TYPES_H
 
+typedef int bool;
+enum { true = 1, false = 0 };
+
 typedef unsigned int  uint;
 typedef unsigned char uchar;
 
