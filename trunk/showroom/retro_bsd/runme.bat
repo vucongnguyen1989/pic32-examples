e7bc11ebbd6fac4eb0e41211472667ea21bd58dc
yuri@panchul.com <yuri@panchul.com@c79ba80f-ef26-09ec-9069-3de584f4e60f>
2012-10-03 04:17:50 +0000

    Moved RetroBSD files around

--- trunk/showroom/retro_bsd/runme.bat
+++ trunk/showroom/retro_bsd/runme.bat
@@ -1,2 +1,2 @@
-avrdude -Cavrdude.conf -c stk500v2 -P COM5 -p pic32 -b 115200 -v -U flash:w:unix.hex:i
-
+cd avrdude
+avrdude -Cavrdude.conf -c stk500v2 -P COM5 -p pic32 -b 115200 -v -U flash:w:../unix.hex:i
