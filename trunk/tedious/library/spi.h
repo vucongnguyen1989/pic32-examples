579947b8b3f1b257d080473d42e347ac328cfbad
yuri@panchul.com <yuri@panchul.com@c79ba80f-ef26-09ec-9069-3de584f4e60f>
2012-12-22 03:06:33 +0000

    Added SPI files to the library

---
+++ trunk/tedious/library/spi.h
@@ -0,0 +1,22 @@
+//  File:   spi.h
+//  Author: Yuri Panchul
+
+#ifndef SPI_H
+#define SPI_H
+
+#include "types.h"
+
+void  spi_init           (void);
+
+uchar spi_put_get_char   (uchar c);
+
+uchar spi_get_char       (void);
+
+void  spi_put_char       (uchar   c);
+void  spi_put_new_line   (void     );
+void  spi_put_str        (uchar * s);
+void  spi_put_dec        (uint    n);
+void  spi_put_hex_digit  (uint    n);
+void  spi_put_hex        (uint    n);
+
+#endif
