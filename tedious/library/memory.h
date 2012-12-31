e6d68fd5786e143b2283ee43976a58e47d13eef5
yuri <yuri@panchul.com>
2012-12-31 10:17:42 +0000

    Implemented memory organization report

---
+++ tedious/library/memory.h
@@ -0,0 +1,9 @@
+//  File:   memory.h
+//  Author: Yuri Panchul
+
+#ifndef MEMORY_H
+#define MEMORY_H
+
+void memory_report (void);
+
+#endif
--- tedious/library/running.h
+++ tedious/library/running.h
@@ -4,10 +4,10 @@
 #ifndef RUNNING_H
 #define RUNNING_H
 
-void running_fast_1 ();
-void running_fast_2 ();
-void running_fast_3 ();
-void running_fast_4 ();
-void running_fast   ();
+void running_fast_1 (void);
+void running_fast_2 (void);
+void running_fast_3 (void);
+void running_fast_4 (void);
+void running_fast   (void);
 
 #endif
