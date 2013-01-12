977b7593fca0dd6ffb808b03ccbae07afbc2c3c4
yuri@panchul.com <yuri@panchul.com@c79ba80f-ef26-09ec-9069-3de584f4e60f>
2013-01-12 04:33:59 +0000

    Separated calculator

---
+++ trunk/tedious/library/calculator.h
@@ -0,0 +1,11 @@
+//  File:   calculator.h
+//  Author: Yuri Panchul
+
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+#include "types.h"
+
+uchar * calculator (uchar in);
+
+#endif
--- trunk/tedious/library/delay.h
+++ trunk/tedious/library/delay.h
@@ -1,6 +1,9 @@
 //  File:   delay.h
 //  Author: Yuri Panchul
 
+#ifndef DELAY_H
+#define DELAY_H
+
 #include "types.h"
 
 void delay_for_1000_nops_x (uint n);
@@ -8,3 +11,5 @@ void delay_for_1000_nops   (void);
 void delay_micros          (uint n);
 void delay_millis          (uint n);
 void delay_seconds         (uint n);
+
+#endif
