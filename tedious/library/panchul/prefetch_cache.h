6f1859c79ed663b7dab07696e6158a39deb58255
yuri <yuri@panchul.com>
2013-09-02 18:52:08 +0000

    Restructured prefetch cache test

---
+++ tedious/library/panchul/prefetch_cache.h
@@ -0,0 +1,10 @@
+//  File:   prefetch_cache.h
+//  Author: Yuri Panchul
+
+#ifndef PREFETCH_CACHE_H
+#define PREFETCH_CACHE_H
+
+#include "types.h"
+
+void prefetch_cache_report (bool dump_cache_lines);
+#endif
