c6d188dba98e1f279a7cd3372e516b15286a9b87
yuri <yuri@panchul.com>
2013-09-11 05:52:22 +0000

    Improved OLED demo

--- tedious/017_oled/main.c
+++ tedious/017_oled/main.c
@@ -10,57 +10,86 @@
 #include "OledChar.h"
 #include "OledGrph.h"
 
+#include "delay.h"
 #include "display.h"
 
-void main ()
+#define width   ccolOledMax
+#define height  crowOledMax
+
+void draw_sin_and_cos ()
 {
-    unsigned i;
-
-#if 0
-    OledInit       ();
-/*
-    OledMoveTo     (20, 10);
-    OledDrawRect   (40, 15);
-    OledPutString  ("abcdefghABCDEFGH");
-    OledUpdate     ();
-*/
-    for (i = 0;; i++)
-    {
-        // OledMoveRight ();
-        // OledMoveDown  ();
-        // OledDrawRect  (40, 15);
-        OledPutChar (i % 5 == 0 ? '\n' : 'a' + i % 26);
-        OledUpdate    ();
-    }
-
-
-
-    display_init ();
-
-    for (i = 0;; i++)
-        display_char ('a' + i % 26);
-#endif
-
     int x;
     double pi = atan (1) * 4;
 
-    OledInit   ();
-    OledMoveTo (0, 16);
-/*
-    OledMoveTo     (20, 10);
-    OledDrawRect   (40, 15);
-    OledPutString  ("abcdefghABCDEFGH");
-    OledUpdate     ();
-*/
+    OledClear ();
 
-    for (x = 0; x < 128; x++)
+    for (x = 0; x < width; x++)
     {
-        int y = 16 - (int) (sin (x * pi * 10 / 128) * 15);
-        OledLineTo (x, y);
+        int y = height / 2 - (int) (sin (x * pi * 10 / width) * (height / 2 - 1));
+
+        if (x != 0)
+            OledLineTo (x, y);
+
         OledMoveTo (x, y);
     }
 
     OledUpdate ();
 
-    for (;;);
+    for (x = 0; x < width; x++)
+    {
+        int y = height / 2 - (int) (cos (x * pi * 3 / width) * (height / 2 - 1));
+
+        if (x != 0)
+            OledLineTo (x, y);
+
+        OledMoveTo (x, y);
+        OledUpdate ();
+    }
+}
+
+void draw_random_rectangles ()
+{
+    int k, i;
+
+    OledClear ();
+
+    for (k = 2; k >= 0; k --)
+    {
+        for (i = 0; i < 100; i ++)
+        {
+            int x = rand () % width;
+            int y = rand () % height;
+
+            OledMoveTo   (x, y);
+            OledDrawRect (x + rand () % (width - x), y + rand () % (height - y));
+
+            OledUpdate ();
+        }
+
+        OledSetDrawMode (modOledXor);
+    }
+}
+
+void demo_character_display ()
+{
+    int i;
+    
+    display_init ();
+
+    for (i = 0; i < 200; i++)
+        display_char ('a' + i % 26);
+}
+
+void main ()
+{
+    unsigned i;
+
+    OledInit ();
+
+    for (;;)
+    {
+        draw_sin_and_cos        ();
+        draw_random_rectangles  ();
+        demo_character_display  ();
+    }
 }
